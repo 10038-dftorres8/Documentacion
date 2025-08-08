857b8426960d84fd6bb713d3cbeaadeac7ca4ced
Jilson Narvaez <janarvaez11@espe.edu.ec>
2025-08-07 23:02:11 -0500

    cambio de estado en préstamo
    Si los contratos no son aprobados

--- src/main/java/com/banquito/Documentacion/client/PrestamoClient.java
+++ src/main/java/com/banquito/Documentacion/client/PrestamoClient.java
@@ -1,7 +1,13 @@
 package com.banquito.Documentacion.client;
+
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.banquito.Documentacion.dto.CrearPrestamoRequest;
 import com.banquito.Documentacion.dto.PrestamoClienteDTO;
 
@@ -9,4 +15,12 @@ import com.banquito.Documentacion.dto.PrestamoClienteDTO;
 public interface PrestamoClient {
   @PostMapping("/api/prestamos/v1/prestamos-clientes")
   PrestamoClienteDTO crearPrestamo(@RequestBody CrearPrestamoRequest req);
+
+  @GetMapping("/api/prestamos/v1/prestamos-clientes/buscar")
+  PrestamoClienteDTO buscarPrestamo(@RequestParam String idCliente, @RequestParam String idPrestamo);
+
+  @PutMapping("/api/prestamos/v1/prestamos-clientes/{id}/estado")
+  PrestamoClienteDTO actualizarEstado(@PathVariable Integer id,
+      @RequestParam String estado);
+
 }
